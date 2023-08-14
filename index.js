377c98eb86057606392f09d8786e0d6469a3dab7
JakeHowdeshell <135374279+JakeHowdeshell@users.noreply.github.com>
2023-08-14 17:38:07 -0500

    updated to display the data from the user input to the README

--- index.js
+++ index.js
@@ -8,68 +8,105 @@ inquirer
     {
       type: "input",
       message: "What is your project title?",
-      name: "name",
+      name: "title",
     },
     {
       type: "input",
       message:
         "Provide a short description explaining the what, why, and how of your project.",
-      name: "input",
+      name: "description",
     },
     {
       type: "confirm",
       message: "Would you like a table of contents?",
-      name: "confirm",
+      name: "tableOfContents",
+      default: true,
     },
     {
       type: "input",
       message:
         "What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.",
-      name: "input",
-    },
-    {
-      type: "confirm",
-      message:
-        "Provide instructions and examples for use. Include screenshots as needed.",
-      name: "confirm",
+      name: "installation",
     },
     {
       type: "input",
+      message:
+        "Provide instructions and examples for use. Include screenshots as needed.",
+      name: "usage",
+    },
+    {
+      type: "list",
       message: "Which licence would you like to use for your project?",
-      name: "input",
+      choices: [ "choice A", "choice B", "choice C", "choice D" ],
+      name: "license",
     },
     {
       type: "input",
       message:
         "List your collaborators, if any, with links to their GitHub profiles.",
-      name: "input",
+      name: "collaborators",
     },
     {
       type: "input",
       message:
         "Go the extra mile and write tests for your application. Then provide examples on how to run them here.",
-      name: "input",
+      name: "test",
     },
     {
       type: "input",
       message: "Provide your Github Username",
-      name: "input",
+      name: "gitHub",
     },
     {
       type: "input",
       message: "Provide your Email address",
-      name: "input",
+      name: "email",
     },
   ])
   .then(function (data) {
-    console.log(JSON.stringify(data));
-    fs.appendFile("README.md", `${JSON.stringify(data)}\n`, (err) =>
+    const response = writeToFile(data);
+    fs.appendFile("README.md", response, (err) =>
       err ? console.error(err) : console.log("Commit logged!")
     );
   });
 
 // TODO: Create a function to write README file
-//function writeToFile(fileName, data) {}
+function writeToFile(data) {
+    return `
+# ${data.title}
+
+## Description\n
+${data.description}
+
+${data.tableOfContents ? `## Table of Contents\n\n` : ''}
+
+${data.tableOfContents ? `- [Installation](#installation)` : ''}
+
+${data.tableOfContents ? `- [Usage](#usage)` : ''}
+
+${data.tableOfContents ? `- [License](#license)` : ''}
+
+${data.tableOfContents ? `- [Contributing](#contributing)` : ''}
+
+${data.tableOfContents ? `- [Tests](#tests)` : ''}
+
+${data.tableOfContents ? `- [Questions](#questions)` : ''}
+
+${data.installation ? `## Installation\n\n${data.installation}\n` : ''}
+
+${data.usage ? `## Usage\n\n${data.usage}\n` : ''}
+
+${data.license ? `## License\n\n${data.license}\n` : ''}
+
+${data.collaborators ? `## Collaborators\n\n${data.collaborators}\n` : ''}
+
+${data.test ? `## Test\n\n${data.test}\n` : ''}
+
+${data.gitHub ? `## Questions\n\n${data.gitHub}\n` : ''}
+
+${data.email ? `${data.email}\n` : ''}
+
+`}
 
 // TODO: Create a function to initialize app
 //function init() {}
